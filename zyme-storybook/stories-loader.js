c07f827b227a9ea25f7d94d43c437713124a0dab
Michal Kedrzynski <kedrzu@gmail.com>
2019-08-08 20:36:28 +0200

    Changes in how story loaders work

--- zyme-storybook/stories-loader.js
+++ zyme-storybook/stories-loader.js
@@ -1,42 +1,56 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
 const path = require('path');
+const { parseQuery } = require('loader-utils');
+
+const storyRegex = /^(([\w-_]*)\.)?([\w-_]*)\.([\w-_]*)\.story$/;
 
 module.exports = function(source) {
     this.cacheable && this.cacheable();
 
-    const stories = yaml.safeLoad(source);
+    if (this.resourceQuery) {
+        const query = parseQuery(this.resourceQuery);
+        if (query.vue) {
+            return source;
+        }
+    }
+
+    const filePath = this.resourcePath;
+    const fileName = path.basename(filePath, '.vue');
+
+    const match = fileName.match(storyRegex);
+
+    const category = match[2];
+    const component = match[3];
+    const story = getStoryName(match[4]);
+
+    const name = category ? `${category}|${component}` : component;
 
     let storyScript = `
 const { withStorySource } = require('@storybook/addon-storysource');
 import { storiesOf } from '@storybook/vue';
+
+const src = ${JSON.stringify(source)};
+const story = require('!!vue-loader!./${fileName}.vue').default;
+    
+storiesOf('${name}', module)
+    .addDecorator(withStorySource(src))
+    .add('${story}', () => story);
 `;
-
-    for (let categoryName of Object.keys(stories)) {
-        const category = stories[categoryName];
-
-        for (let storyName of Object.keys(category)) {
-            const storyFile = category[storyName];
-
-            storyScript += buildStoryScript({
-                categoryName,
-                storyName,
-                storyFile
-            });
-        }
-    }
 
     return storyScript;
 };
 
-function buildStoryScript({ categoryName, storyName, storyFile }) {
-    return `
-{
-    const src = require('!!raw-loader!${storyFile}').default;
-    const story = require('${storyFile}').default;
-    
-    storiesOf('${categoryName}', module)
-        .addDecorator(withStorySource(src))
-        .add('${storyName}', () => story);
-}`;
+function getStoryName(name) {
+    // first letter will always be upper case
+    let result = name[0].toUpperCase();
+    for (let i = 1; i < name.length; i++) {
+        let letter = name[i];
+        let isUpperCase = letter == letter.toUpperCase();
+        if (isUpperCase) {
+            result += ' ' + letter.toLowerCase();
+        } else {
+            result += letter;
+        }
+    }
+
+    return result;
 }
