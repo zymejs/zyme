97bc47adf114a1fd2d2911558ad16bd4dc0a045c
Michał Kędrzyński <m.kedrzynski@perfectgym.com>
2019-10-05 23:24:30 +0200

    Changes in prototypes

--- zyme-patterns/src/TypeMap.ts
+++ zyme-patterns/src/TypeMap.ts
@@ -1,22 +1,22 @@
-import { Prototype, Typed } from './Prototype';
+import { Prototype, Typed, TypeKey } from './Prototype';
 
 type TypeMapEntry<T> = [Prototype | string, T];
 
 export class TypeMap<T> {
-    private readonly map: Map<string, T>;
+    private readonly map: Map<TypeKey, T>;
 
     constructor(entries?: Array<TypeMapEntry<T>>) {
         if (entries) {
-            this.map = new Map<string, T>(entries.map(toMapEntry));
+            this.map = new Map(entries.map(toMapEntry));
         } else {
-            this.map = new Map<string, T>();
+            this.map = new Map();
         }
     }
 
-    public get(typeId: string): T | undefined;
+    public get(typeId: TypeKey): T | undefined;
     public get(object: Typed): T | undefined;
     public get(proto: Prototype): T | undefined;
-    public get(param: string | Typed | Prototype): T | undefined {
+    public get(param: TypeKey | Typed | Prototype): T | undefined {
         if (isTypeId(param)) {
             return this.map.get(param);
         } else if (param instanceof Prototype) {
@@ -26,10 +26,10 @@ export class TypeMap<T> {
         }
     }
 
-    public set(typeId: string, value: T): void;
+    public set(typeId: TypeKey, value: T): void;
     public set(object: Typed, value: T): void;
     public set(proto: Prototype, value: T): void;
-    public set(param: string | Typed | Prototype, value: T): void {
+    public set(param: TypeKey | Typed | Prototype, value: T): void {
         if (isTypeId(param)) {
             this.map.set(param, value);
         } else if (param instanceof Prototype) {
@@ -40,7 +40,7 @@ export class TypeMap<T> {
     }
 }
 
-function toMapEntry<T>(entry: TypeMapEntry<T>): [string, T] {
+function toMapEntry<T>(entry: TypeMapEntry<T>): [TypeKey, T] {
     if (isTypeId(entry[0])) {
         return entry as [string, T];
     }
@@ -48,6 +48,6 @@ function toMapEntry<T>(entry: TypeMapEntry<T>): [string, T] {
     return [entry[0].type, entry[1]];
 }
 
-function isTypeId(param: any): param is string {
-    return typeof param === 'string';
+function isTypeId(param: any): param is TypeKey {
+    return typeof param === 'string' || typeof param === 'symbol';
 }
