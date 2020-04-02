1c942a58d898d4f6af0f6eff8c92d744b7b0d83c
Michał Kędrzyński <kedrzu@gmail.com>
2020-04-02 13:52:03 +0200

    Temporarily removed ref _brand fix

--- zyme-types/VueHelpers.d.ts
+++ zyme-types/VueHelpers.d.ts
@@ -21,6 +21,6 @@ declare module '@vue/composition-api/dist/reactivity/ref' {
     const _refBrand: unique symbol;
     // from https://github.com/vuejs/composition-api/pull/199
     interface Ref<T> {
-        readonly [_refBrand]: true;
+        readonly [_refBrand]?: true;
     }
 }
