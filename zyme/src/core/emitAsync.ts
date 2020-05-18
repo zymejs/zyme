3bc9775854da49b71770303be87b897d0e22bbed
Michał Kędrzyński <kedrzu@gmail.com>
2020-05-18 13:45:22 +0200

    Added separate emitAsync function

--- zyme/src/core/emitAsync.ts
+++ zyme/src/core/emitAsync.ts
@@ -1,23 +1,23 @@
 import { requireCurrentInstance } from './helpers';
 
-type EmitAsyncCallback = (event: string, arg?: any) => Promise<void>;
-
-export function useEmitAsync(): EmitAsyncCallback {
+export function useEmitAsync() {
     const vm = requireCurrentInstance();
 
-    return (event, arg) => {
-        const listeners = vm.$listeners && vm.$listeners[event];
+    return (event: string, arg?: any) => emitAsync(vm, event, arg);
+}
 
-        // no listeners available
-        if (!listeners) {
-            return Promise.resolve();
-        }
+export function emitAsync(vm: Vue, event: string, arg?: any): Promise<void> {
+    const listeners = vm.$listeners && vm.$listeners[event];
 
-        if (Array.isArray(listeners)) {
-            // there are many listeners for this event
-            return Promise.all(listeners.map(s => s(arg))) as Promise<any>;
-        } else {
-            return Promise.resolve(listeners(arg)) as Promise<void>;
-        }
-    };
+    // no listeners available
+    if (!listeners) {
+        return Promise.resolve();
+    }
+
+    if (Array.isArray(listeners)) {
+        // there are many listeners for this event
+        return Promise.all(listeners.map((s) => s(arg))) as Promise<any>;
+    } else {
+        return Promise.resolve(listeners(arg)) as Promise<void>;
+    }
 }
