8a363a962183e78c1009f50d41505198518a8400
Michał Kędrzyński <m.kedrzynski@perfectgym.com>
2020-07-13 17:41:41 +0200

    Changed useEventBus to use vue instance underneath

--- zyme/src/utils/useEventBus.ts
+++ zyme/src/utils/useEventBus.ts
@@ -1,46 +1,40 @@
+import Vue from 'vue';
+import { getCurrentInstance } from '@vue/composition-api';
+
 import { onActivated, onBeforeUnmount, onDeactivated } from '../core';
 
 type EventCallback<T = void> = (arg: T) => void;
 
-const events: { [event: string]: EventCallback<any>[] } = {};
+const bus = new Vue();
 
 export function useEventBus() {
     return {
         on<T = void>(event: string, fct: EventCallback<T>) {
             on(event, fct);
-            onDeactivated(() => off(event, fct));
-            onActivated(() => on(event, fct));
-            onBeforeUnmount(() => off(event, fct));
+
+            const vm = getCurrentInstance();
+            if (vm) {
+                onDeactivated(() => off(event, fct));
+                onActivated(() => on(event, fct));
+                onBeforeUnmount(() => off(event, fct));
+            }
         },
         emit,
     };
 }
 
 function on<T = void>(event: string, fct: EventCallback<T>) {
-    const e = event as string;
-    events[e] = events[e] || [];
-    events[e].push(fct);
+    bus.$on(event, fct);
 
     return () => off(event, fct);
 }
 
 function off<T = void>(event: string, fct: EventCallback<T>) {
-    const e = event as string;
-    const listeners = events[e];
-    if (!listeners) {
-        return;
-    }
-
-    listeners.splice(listeners.indexOf(fct), 1);
+    bus.$off(event, fct);
 }
 
-function emit<T = void>(event: string, arg: T): void {
-    const listeners = events[event as string];
-    if (!listeners) {
-        return;
-    }
-
-    for (const cb of listeners) {
-        cb(arg);
-    }
+function emit(event: string): void;
+function emit<T>(event: string, arg: T): void;
+function emit<T>(event: string, arg?: T): void {
+    bus.$emit(event, arg);
 }
