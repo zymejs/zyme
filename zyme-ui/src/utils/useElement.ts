bde9e060fe33b913f15f2f8aaa1d41070fc3a2e9
Michał Kędrzyński <kedrzu@gmail.com>
2020-11-16 17:23:10 +0100

    useElement is generic now

--- zyme-ui/src/utils/useElement.ts
+++ zyme-ui/src/utils/useElement.ts
@@ -1,7 +1,7 @@
-import { computed, getCurrentInstance } from '@vue/composition-api';
+import { computed, getCurrentInstance, Ref } from '@vue/composition-api';
 
-export function useElement() {
+export function useElement<T extends Element>(): Readonly<Ref<T | undefined>> {
     const vm = getCurrentInstance();
 
-    return computed(() => vm?.$el);
+    return computed(() => vm?.$el as T | undefined);
 }
