17d9ff3ab95aa5dd328f714b98e40c04a96944a3
Michał Kędrzyński <kedrzu@gmail.com>
2020-11-17 10:38:32 +0100

    Extended useGlobalClass

--- zyme-ui/src/utils/useGlobalClass.ts
+++ zyme-ui/src/utils/useGlobalClass.ts
@@ -1,14 +1,49 @@
-import { onMounted, onUnmounted } from '@vue/composition-api';
+import { RefParam, toRef } from 'zyme';
+import { onMounted, onUnmounted, watch } from '@vue/composition-api';
 
-export function useGlobalClass(cssClass: string, ...elements: (Element | undefined | null)[]) {
+interface GlobalClassOptions {
+    elements: (Element | undefined | null)[];
+    condition?: RefParam<boolean>;
+}
+
+interface GlobalClassOptionsSingle extends GlobalClassOptions {
+    class: string;
+}
+
+interface GlobalClassOptionsMulti extends GlobalClassOptions {
+    classes: string[];
+}
+
+export function useGlobalClass(options: GlobalClassOptionsSingle | GlobalClassOptionsMulti) {
+    const classes = (options as GlobalClassOptionsMulti).classes ?? [
+        (options as GlobalClassOptionsSingle).class,
+    ];
+    const elements = options.elements;
+
     onMounted(() => {
-        for (const element of elements) {
-            element?.classList.add(cssClass);
+        if (options.condition) {
+            watch(
+                toRef(options.condition),
+                (condition) => (condition ? addClass() : removeClass()),
+                { immediate: true }
+            );
+        } else {
+            // no condition - add class immadietely
+            addClass();
         }
     });
-    onUnmounted(() => {
+
+    onUnmounted(removeClass);
+
+    function addClass() {
         for (const element of elements) {
-            element?.classList.remove(cssClass);
+            element?.classList.add(...classes);
         }
-    });
+    }
+
+    function removeClass() {
+        for (const element of elements) {
+            element?.classList.remove(...classes);
+        }
+    }
 }
