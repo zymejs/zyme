32f942f85f2f60236ec7f8ec5e91a53dc60f0a5c
Michał Kędrzyński <kedrzu@gmail.com>
2021-04-27 08:44:27 +0200

    fix: useElement was caching undefined in some cases

--- zyme-ui/src/utils/useElement.ts
+++ zyme-ui/src/utils/useElement.ts
@@ -1,7 +1,11 @@
 import { computed, getCurrentInstance, Ref } from '@vue/composition-api';
 
 export function useElement<T extends Element>(): Readonly<Ref<T | undefined>> {
-    const vm = getCurrentInstance();
+    const vm = getCurrentInstance()?.proxy;
 
-    return computed(() => vm?.proxy?.$el as T | undefined);
+    return {
+        get value() {
+            return vm?.$el as T | undefined;
+        },
+    } as Readonly<Ref<T | undefined>>;
 }
